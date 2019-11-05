352b9f8dcd8c153606bb25ca4064a48bd43e6b4a
Daniel O'Connell <tojad99@gmail.com>
2019-11-06 00:09:16 +0100

    Add configs

---
+++ project.clj
@@ -0,0 +1,66 @@
+(defproject herbarium "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.520"
+                  :exclusions [com.google.javascript/closure-compiler-unshaded
+                               org.clojure/google-closure-library]]
+                 [thheller/shadow-cljs "2.8.69"]
+                 [reagent "0.8.1"]
+                 [re-frame "0.10.9"]
+                 [garden "1.3.9"]
+                 [ns-tracker "0.4.0"]
+                 [compojure "1.6.1"]
+                 [yogthos/config "1.1.6"]
+                 [tongue "0.2.8"]
+                 [ring "1.7.1"]]
+
+  :plugins [[lein-garden "0.3.0"]
+            [lein-shell "0.5.0"]]
+
+  :min-lein-version "2.5.3"
+
+  :jvm-opts ["-Xmx1G"]
+
+  :source-paths ["src/clj" "src/cljs"]
+
+  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"
+                                    "resources/public/css"]
+
+
+  :garden {:builds [{:id           "screen"
+                     :source-paths ["src/clj"]
+                     :stylesheet   herbarium.css/screen
+                     :compiler     {:output-to     "resources/public/css/screen.css"
+                                    :pretty-print? true}}]}
+
+  :shell {:commands {"open" {:windows ["cmd" "/c" "start"]
+                             :macosx  "open"
+                             :linux   "xdg-open"}}}
+
+  :aliases {"dev"          ["with-profile" "dev" "do"
+                            ["clean"]
+                            ["run" "-m" "shadow.cljs.devtools.cli" "watch" "app"]]
+            "prod"         ["with-profile" "prod" "do"
+                            ["clean"]
+                            ["run" "-m" "shadow.cljs.devtools.cli" "release" "app"]]
+            "build-report" ["with-profile" "prod" "do"
+                            ["clean"]
+                            ["run" "-m" "shadow.cljs.devtools.cli" "run" "shadow.cljs.build-report" "app" "target/build-report.html"]
+                            ["shell" "open" "target/build-report.html"]]
+            "karma"        ["with-profile" "prod" "do"
+                            ["clean"]
+                            ["run" "-m" "shadow.cljs.devtools.cli" "compile" "karma-test"]
+                            ["shell" "karma" "start" "--single-run" "--reporters" "junit,dots"]]}
+
+  :profiles
+  {:dev
+   {:dependencies [[binaryage/devtools "0.9.10"]]}
+
+   :prod { }
+
+   :uberjar {:source-paths ["env/prod/clj"]
+             :omit-source  true
+             :main         herbarium.server
+             :aot          [herbarium.server]
+             :uberjar-name "herbarium.jar"
+             :prep-tasks   ["compile" ["prod"]["garden" "once"]]}
+   })
